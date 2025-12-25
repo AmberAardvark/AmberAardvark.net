4df806c03c892e481778d15c24d6dd82a4f2454b
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2025-12-24 21:10:06 -0600

    Update site URL and add base path in astro.config.mjs; Create robots.txt for web crawling control

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,8 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://AmberAardvark.net",
+  site: "https://AmberAardvark.github.io",
+  base: "/AmberAardvark.net",
   image: {
     domains: ["images.unsplash.com"],
   },
